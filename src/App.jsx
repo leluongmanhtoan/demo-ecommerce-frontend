90d7410051ce80f1c5f7696278f121fbf2dc0a8f
Toan Le <leluongmanhtoan@gmail.com>
2024-10-19 17:52:00 +0700

    update - route page, navigator, menu user, fix missing categories after filter, change logo demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
 import Header from './components/header/header'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home/index'
+import About from './pages/About/index'
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header/>
-    </>
+      <Routes>
+        <Route exact={true} path="/" element={<Home/>}/>
+        <Route exact={true} path="/about" element={<About/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/components/selectDrop/select.jsx
+++ src/components/selectDrop/select.jsx
@@ -1,34 +1,60 @@
 import React, { useState } from 'react';
 import '../selectDrop/select.css'
-const Select = () => {
-    const [isOpenSelect, setisOpenSelect] = useState(true)
-    const openSelect=()=>{
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { ClickAwayListener } from '@mui/material';
+const Select = ({ data, placeholder,icon }) => {
+    const [isOpenSelect, setisOpenSelect] = useState(false);
+    const [selectedIndex, setSelectedIndex] = useState(0);
+    const [selectedItem, setSelectedItem] = useState(placeholder);
+    const [listData, setListData] = useState(data)
+    const [listData2, setListData2] = useState(data)
+    const openSelect = () => {
         setisOpenSelect(!isOpenSelect);
     }
+    const closeSelect = (index, name) => {
+        setSelectedIndex(index);
+        setisOpenSelect(false);
+        setSelectedItem(name);
+    }
+
+    const filterList = (e)=>{
+        const keyword = e.target.value.toLowerCase();
+        const list = listData2.filter((item)=>{
+            return item.toLowerCase().includes(keyword)
+        })
+
+        const list2 = list.filter((item,index)=>list.indexOf(item)===index);
+        
+        setListData(list2)
+        
+    }
     return (
-        <div className='selectDropWrapper cursor posision-relative'>
-            <span className='openSelect' onClick={openSelect}>All Catagories</span>
-            
-            {
-                isOpenSelect === true &&
-                <div className='selectDrop'>
-                <div className='searchField'>
-                    <input type="text" />
-                    <ul className='searchResults'>
-                        <li>Milks and Dairies</li>
-                        <li>Wines & Drinks</li>
-                        <li>Clothing & Beauty</li>
-                        <li>Fresh Seafood</li>
-                        <li>Pet Foods & Toy</li>
-                        <li>Fast Food</li>
-                        <li>Baking Material</li>
-                        <li>Vegetables</li>
-                        <li>Fresh Fruit</li>
-                    </ul>
-                </div>
+        <ClickAwayListener onClickAway={() => setisOpenSelect(false)}>
+            <div className='selectDropWrapper cursor posision-relative'>
+                {icon}
+                <span className='openSelect' onClick={openSelect}>{selectedItem.length > 14 ? selectedItem.substr(0,14) + '...':selectedItem} <KeyboardArrowDownIcon className='arrow' /></span>
+
+                {
+                    isOpenSelect === true &&
+                    <div className='selectDrop'>
+                        <div className='searchField'>
+                            <input type="text" placeholder='Search here...' onChange={filterList}/>
+                            <ul className='searchResults'>
+                                <li key={0} onClick={() => closeSelect(0, placeholder)} className={`${selectedIndex === 0 ? 'active' : ''}`}>{placeholder}</li>
+                                {
+                                    listData.map((item, index) => {
+                                        return (
+                                            <li key={index + 1} onClick={() => closeSelect(index + 1, item)} className={`${selectedIndex === index + 1 ? 'active' : ''}`}>{item}</li>
+                                        )
+                                    })
+                                }
+                            </ul>
+                        </div>
+                    </div>
+                }
             </div>
-            }
-        </div>
+        </ClickAwayListener>
+
     )
 }
 export default Select
